e924b8a795b8047644b519a8ffb2b31ab26ca582
UttamkiniH <huttamkini23@gmail.com>
2021-06-27 12:54:41 +0530

    introduced statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,29 @@ void main() => runApp(MyApp());
 //MyApp is class name which is taking reference from material.dart package(inherintence)
 // build is method which builds the whole app this method requires context as argument
 //MaterialApp is class
-class MyApp extends StatelessWidget {
+var question = [
+  'How old are you?',
+  'Do you like chicken?',
+];
+
+var questionIndex = 0;
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  void answerQuestion() {
+    setState(() {
+      questionIndex += 1;
+    });
+    print(questionIndex);
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
@@ -18,18 +40,20 @@ class MyApp extends StatelessWidget {
           ),
           body: Column(
             children: [
-              Text('Question appears here'),
+              //use . for accessing element
+              Text(question[questionIndex]),
               RaisedButton(
                 child: Text('Answer 1'),
-                onPressed: null,
+                onPressed: answerQuestion,
+                //the function only has to work when the user presess the button so avoid using ()
               ),
               RaisedButton(
                 child: Text('Answer 2'),
-                onPressed: null,
+                onPressed: answerQuestion,
               ),
               RaisedButton(
                 child: Text('Answer 3'),
-                onPressed: null,
+                onPressed: answerQuestion,
               ),
             ],
           )),
